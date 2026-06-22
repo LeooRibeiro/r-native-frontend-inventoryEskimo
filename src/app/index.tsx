b8183f4dc486602a0a7b4ac7bbaf14131b25fab7
Leonardo <leo.ribeirobosco@gmail.com>
2026-06-22 01:08:17 -0300

    Initial commit with base configs

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import '../styles/global.css'
+
+export default function Index() {
+  return (
+    <View
+    >
+      <Text className="text-red-500 font-bold">Defenility Hello World ha ha haa</Text>
+    </View>
+  );
+}
